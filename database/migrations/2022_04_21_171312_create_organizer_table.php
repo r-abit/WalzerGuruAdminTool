9d12fe27e4db175ddbd9b47d3da3be8ea2ce0eb4
rabit_ <r.ljatifi@droidmarketing.com>
2022-04-21 21:48:40 +0200

    added migration and model for organizers

---
+++ database/migrations/2022_04_21_171312_create_organizer_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('organizers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->string('website');
+            $table->string('uid_number', 15);
+            $table->string('street');
+            $table->string('zip', 10);
+            $table->string('city', 20);
+            $table->string('phone', 15);
+            $table->longText('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('organizers');
+    }
+};
