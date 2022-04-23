af3c585997cd71e3c011fe63af823cfd0a90f38b
rabit_ <r.ljatifi@droidmarketing.com>
2022-04-24 01:45:38 +0200

    changed date to timestamp on migration

---
+++ database/migrations/2022_04_23_230415_create_event_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('organizer_id')->nullable();
+            $table->foreign('organizer_id')->references('id')->on('organizers')->nullOnDelete();
+            $table->string('name');
+            $table->integer('participants');
+            $table->timestamp('date');
+            $table->string('dresscode');
+            $table->string('street');
+            $table->string('zip');
+            $table->string('city');
+            $table->longText('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+};
