f106590fff94931ce1dcee8d0f5f9978b3a214ad
rabit_ <r.ljatifi@droidmarketing.com>
2022-04-26 13:42:27 +0200

    added delete button in the frontend

--- resources/js/Pages/Organizer/Partials/OrganizerList.vue
+++ resources/js/Pages/Organizer/Partials/OrganizerList.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { Link } from '@inertiajs/inertia-vue3'
 
 const props = defineProps({
     organizers: Object,
@@ -41,27 +42,46 @@ const updateForm = (organizer) => {
                 <th class="px-3 py-3 text-xs font-semibold text-gray-600 uppercase tracking-wider">
                     E-Mail
                 </th>
+                <th class="px-3 py-3 text-xs font-semibold text-center text-gray-600 uppercase tracking-wider">
+                    Löschen
+                </th>
             </tr>
         </thead>
         <tbody>
-            <tr v-for="organizer in organizers" @click="updateForm(organizer)" class="text-sm text-left border-b border-gray-200 cursor-pointer hover:bg-gray-100">
-                <td class="px-3 py-3">
+            <tr v-for="organizer in organizers" class="text-sm text-left border-b border-gray-200 hover:bg-gray-100">
+                <td class="px-3 py-3 cursor-pointer" @click="updateForm(organizer)">
                     {{ organizer.name }}
                 </td>
-                <td class="px-3 py-3">
+                <td class="px-3 py-3 cursor-pointer" @click="updateForm(organizer)">
                     {{ organizer.street }}
                 </td>
-                <td class="px-3 py-3">
+                <td class="px-3 py-3 cursor-pointer" @click="updateForm(organizer)">
                     {{ organizer.zip }}
                 </td>
-                <td class="px-3 py-3">
+                <td class="px-3 py-3 cursor-pointer" @click="updateForm(organizer)">
                     {{ organizer.city }}
                 </td>
-                <td class="px-3 py-3">
+                <td class="px-3 py-3 cursor-pointer" @click="updateForm(organizer)">
                     {{ organizer.phone }}
                 </td>
-                <td class="px-3 py-3">
+                <td class="px-3 py-3 cursor-pointer" @click="updateForm(organizer)">
                     {{ organizer.email }}
+                </td>
+                <td class="px-3 py-3 flex justify-center items-center ">
+                    <Link href="/organizers" method="delete" :data="{ id: organizer.id }">
+                        <svg
+                             xmlns="http://www.w3.org/2000/svg"
+                             class="p-1 rounded-full h-7 w-7 hover:bg-red-300 cursor-pointer"
+                             fill="none"
+                             viewBox="0 0 24 24"
+                             stroke="currentColor"
+                             stroke-width="2">
+                                <path
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                        </svg>
+                    </Link>
                 </td>
             </tr>
 
