ffa9392a02f4a5639ca3e95ae5afe30bd19f680c
rabit_ <r.ljatifi@droidmarketing.com>
2022-05-08 23:01:30 +0200

    events are now passed to fellow web

--- app/Http/Controllers/FellowController.php
+++ app/Http/Controllers/FellowController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Event;
 use Illuminate\Http\Request;
 use Laravel\Jetstream\Jetstream;
 
@@ -11,6 +12,7 @@ class FellowController extends Controller
     public function show(Request $request): \Inertia\Response
     {
         return Jetstream::inertia()->render($request, 'Fellow/Show', [
+            'events' => Event::get()->toArray(),
         ]);
     }
 
