4179eb6a681708ccb791698744518dee412dc902
rabit_ <r.ljatifi@droidmarketing.com>
2022-05-13 17:09:33 +0200

    added missing events to the response

--- app/Http/Controllers/DancingController.php
+++ app/Http/Controllers/DancingController.php
@@ -7,14 +7,16 @@ use Illuminate\Support\Facades\Auth;
 use Laravel\Jetstream\Jetstream;
 use Illuminate\Http\Request;
 use App\Models\Organizer;
+use Inertia\Response;
 
 class DancingController extends Controller
 {
 
-    public function show(Request $request): \Inertia\Response
+    public function show(Request $request): Response
     {
         $events = array();
-        foreach (Auth::user()->events as $user_event) array_push($events, $user_event->event->toArray());
+        foreach (Auth::user()->events as $user_event)
+            $events[] = $user_event->event->toArray();
 
         // Sort event date ASC
         $col = array_column( $events, 'date' );
@@ -24,8 +26,10 @@ class DancingController extends Controller
         $upcoming_events = array();
         foreach ($events as $event){
             $date_now = date('Y-m-d H:i:s',strtotime('now'));
-            if ($event['date'] > $date_now) array_push($upcoming_events, $event);
-            else array_push($previous_events, $event);
+            if ($event['date'] > $date_now)
+                $upcoming_events[] = $event;
+            else
+                $previous_events[] = $event;
         }
 
         return Jetstream::inertia()->render($request, 'Dancing/Show', [
@@ -36,17 +40,37 @@ class DancingController extends Controller
         ]);
     }
 
-     public function delete(Request $request): \Inertia\Response
+     public function delete(Request $request): Response
      {
+         $events = array();
+         foreach (Auth::user()->events as $user_event)
+             $events[] = $user_event->event->toArray();
+
+         // Sort event date ASC
+         $col = array_column( $events, 'date' );
+         array_multisort( $col, SORT_ASC, $events );
+
+         $previous_events = array();
+         $upcoming_events = array();
+         foreach ($events as $event){
+             $date_now = date('Y-m-d H:i:s',strtotime('now'));
+             if ($event['date'] > $date_now)
+                 $upcoming_events[] = $event;
+             else
+                 $previous_events[] = $event;
+         }
+
          EventParticipation::where('event_id', $request->id)
              ->where('user_id', Auth::id())
              ->delete();
 
          $events = array();
-         foreach (Auth::user()->events as $user_event) array_push($events, $user_event->event->toArray());
+         foreach (Auth::user()->events as $user_event) $events[] = $user_event->event->toArray();
 
          return Jetstream::inertia()->render($request, 'Dancing/Show', [
             'events' => $events,
+            'previous_events' => $previous_events,
+            'upcoming_events' => $upcoming_events,
             'organizers' => Organizer::get()->toArray(),
         ]);
      }
