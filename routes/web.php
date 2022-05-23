c6cdfd7fdd48e0ce7b2f03a1a3eaef664a7b7a3f
rabit_ <r.ljatifi@droidmarketing.com>
2022-05-23 04:06:20 +0200

    redirect / to /dashboard to bypass welcome page

--- routes/web.php
+++ routes/web.php
@@ -19,12 +19,13 @@ use Inertia\Inertia;
 */
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+    return redirect()->route('dashboard');
+//    return Inertia::render('Welcome', [
+//        'canLogin' => Route::has('login'),
+//        'canRegister' => Route::has('register'),
+//        'laravelVersion' => Application::VERSION,
+//        'phpVersion' => PHP_VERSION,
+//    ]);
 });
 
 Route::middleware([
