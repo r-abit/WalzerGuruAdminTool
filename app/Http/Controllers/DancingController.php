5b739535c625d37a8db2ff25a95c0c04221b7142
rabit_ <r.ljatifi@droidmarketing.com>
2022-06-15 16:26:50 +0200

    added also list for not liked dancers in the controller

--- app/Http/Controllers/DancingController.php
+++ app/Http/Controllers/DancingController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\DancingLevel;
 use App\Models\EventParticipation;
 use App\Models\LikedUsers;
+use App\Models\PreviousDancePartner;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Laravel\Jetstream\Jetstream;
@@ -20,11 +21,9 @@ class DancingController extends Controller
          * Get all the previous dancing partners.
          * Liked and also the rest.
          * As separate lists.
-         * TODO: Migration and seeder for the rest of the dancing partners!
          */
         $liked_by_id = LikedUsers::where('user', Auth::id())->get()->toArray();
         $liked_users = array();
-        $not_liked_users = array();
         $fields = ['id', 'username', 'height', 'dancing_level', 'birthday', 'profile_photo_path'];
         $dancing_levels = DancingLevel::get()->toArray();
         foreach ($liked_by_id as $id){
@@ -35,11 +34,32 @@ class DancingController extends Controller
                     break;
                 }
             }
-            $user['liked'] = false;
-            $not_liked_users[] = $user;
             $user['liked'] = true;
             $liked_users[] = $user;
         }
+
+        $not_liked_users = $liked_users;
+        $liked_users_id = array_column($liked_users, 'id');
+        foreach(PreviousDancePartner::where('user', Auth::id())->get()->toArray() as $dance_partner) {
+            $user = User::where('id', $dance_partner['id'])->first($fields)->toArray();
+            foreach ($dancing_levels as $level){
+                if ($level['id'] == $user['dancing_level']) {
+                    $user['dancing_level'] = $level['level'];
+                    break;
+                }
+            }
+
+            if(!in_array($dance_partner['id'], $liked_users)){
+                $user['liked'] = false;
+                $not_liked_users[] = $user;
+            }
+        }
+        usort($not_liked_users, function($a, $b) {
+            return $a['username'] <=> $b['username'];
+        });
+        usort($liked_users, function($a, $b) {
+            return $a['username'] <=> $b['username'];
+        });
 
         return Jetstream::inertia()->render($request, 'Dancing/Show', [
             'liked_users' => $liked_users,
